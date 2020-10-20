d0739a7543a93587ad908a4e5eff8144d26ec93e
fiddy-rizarta <fiddy.root@gmail.com>
2020-10-20 21:08:07 +0700

    started at 20-10-2020

---
+++ application/helpers/key_helper.php
@@ -0,0 +1,12 @@
+<?php
+    function getServerKey()
+    {
+        // return "SB-Mid-server-LGhgyGOtmzoC20KGT0JVr_w7";
+        return "SB-Mid-server-Ee9g8BK7G0bvr52dj9TKWUG5";
+    }
+
+    function getClientKey(){
+        // return "SB-Mid-client-4TFhB5uD0B6Toeai";
+        return "SB-Mid-client-0p8lbY7CC1VMsL_y";
+    }
+?>
